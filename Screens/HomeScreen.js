b0332aa66b4c83c50d875bf0bd446318a0eba954
Sammy John Rawlinson <sraw211@mywhitecliffe.com>
2022-09-19 10:50:28 +1200

    feat
    Added HomeScreen and LogInScreen

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import { useNavigation } from '@react-navigation/core'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react';
+
+const HomeScreen = () => {
+    const navigation = useNavigation()
+
+
+
+return (
+    <View style={styles.container}>
+        <Text>Email: </Text>
+        <TouchableOpacity
+            onPress= {""}
+            style={styles.button}
+        >
+            <Text style={styles.buttonText}>Sign Out</Text>
+        </TouchableOpacity>
+    </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '60%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+})
